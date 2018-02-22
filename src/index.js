6f9348aeae7fefc2894e1f64429ff080691df6c5
Ilhan Adiyaman <ilhanadiyamann@gmail.com>
2018-02-22 22:59:28 +0100

    react added.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Minimal setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Minimal Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
